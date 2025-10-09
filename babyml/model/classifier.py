5f8c407c860ad18f916edecaf32cd0c0abc5a48e
goyalpramod <goyalpramod1729@gmail.com>
2025-10-08 20:51:04 -0400

    feat: add gini impurity and entropy

--- babyml/model/classifier.py
+++ babyml/model/classifier.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 class LogisticRegression:
     def __init__(self):
         self.coefficients = None
@@ -22,8 +24,61 @@ class NaiveBayesClassifier:
 
 
 class DecisionTreeClassifier:
-    def __init__(self):
-        self.tree = None
+    """
+    Find the mathematical formulation used by sklearn here: https://scikit-learn.org/stable/modules/tree.html#mathematical-formulation
+    This part from a course by CMU was extremely helpful: https://www.cs.cmu.edu/~bhiksha/courses/10-601/decisiontrees/
+    And this blog too: https://machinelearningmastery.com/implement-decision-tree-algorithm-scratch-python/
+    """
+    def __init__(
+        self,
+        criterion : str ="gini",
+        max_depth : Optional[int] = None,
+        min_samples_split : int = 2,
+        max_features : Optional[int] = None,
+        random_seed : int = 42,):
+        """        
+        Parameters
+        ----------
+        criterion : {'gini', 'entropy'}, default='gini'
+            The function to measure split quality.
+        """
+        self.criterion = criterion
+    
+    def fit(self, X, y):
+        len_attributes = len(X[0])
+        len_instance = len(X)
+
+
+    def calculate_gini_index(self, y):
+        """
+        The mathematical formula for gini index is given by 
+
+        """
+        gini_impurity = 1
+        unique_labels = set(y)
+        n = len(y)
+        for label in unique_labels:
+            count = y.count(label)  
+            probability = count / n
+            gini_impurity -= probability ** 2
+        return gini_impurity
+
+    def calculate_entropy(self,y):
+        from math import log
+
+        entropy = 0
+        unique_labels = set(y)
+        n = len(y)
+        for label in unique_labels:
+            count = y.count(label)
+            prob = count/n
+            entropy -= prob*log(prob)
+        return entropy
+
+    def calculate_information_gain(self):
+        pass
+
+
 
 class KNNClassifier:
     def __init__(self, n_neighbors=5):
